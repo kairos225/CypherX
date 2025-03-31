38d92c38501d6d1fbe64d6df2331673bf3bf464a
kairos225 <puissance4b@gmail.com>
2025-03-31 11:14:31 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'XPLOADER-BOT:~mjQEWKIB#RN9hAu34_p7bs5Y0n08USU5VfaHC9FWMeQw-cEtD2qM' 
 // You know the drill. Put your CypherX session id here. Should look like: XPLOADER-BOT:~ (don't forget that, it's crucial!)
 
 //--------------------[ BOT NAME ]----------------------//
@@ -29,17 +29,17 @@ global.botname = process.env.BOT_NAME || 'CypherX'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '254754783972' 
+global.ownernumber = process.env.OWNER_NUMBER || '2250564449519' 
 // Your personal contact number. If you want to call Tylor, this is the number. 👀
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Tylor' 
+global.ownername = process.env.OWNER_NAME || '𝛫𝛥𝛪𝑅𝛩𝑆 𝛮𝛩𝛻𝛥𝐶𝛨𝑅𝛩𝛮𝛩𝑆👾❤‍🔥❤‍🔥❤‍🔥' 
 // Set your name, currently it's the mastermind's. The one and only... Tylor. (Don't ask how I got here, it's a mystery.)
 
 //------------[ STICKER PACKNAME ]-----------------//
 
-global.packname = process.env.STICKER_PACK_NAME || "Cypher" 
+global.packname = process.env.STICKER_PACK_NAME || "𝛫𝛥𝛪𝑅𝛩𝑆 𝛮𝛩𝛻𝛥𝐶𝛨𝑅𝛩𝛮𝛩𝑆👾❤‍🔥❤‍🔥❤‍🔥" 
 // Time to decide the name for your sticker pack. This will appear every time you create a sticker. "Cypher" is the default because... well, why not?
 
 //--------------[ STICKER AUTHOR NAME ]------------//
